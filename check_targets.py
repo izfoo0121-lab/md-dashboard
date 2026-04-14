83728bbc358dd3473e8d852512ac1ffe78272d5b
Isaac <izfoo0121@gmail.com>
2026-04-14 16:08:26 +0800

    Remove temp/debug files from repo

--- check_targets.py
+++
@@ -1,9 +0,0 @@
-import json
-t = json.load(open('targets.json', encoding='utf-8'))
-
-# Check monthly_targets for Mar 26
-mt = t.get('monthly_targets', {}).get('Mar 26', {})
-print("Monthly targets Mar 26 agents:", list(mt.keys()))
-print()
-cj = mt.get('CJ', {})
-print("CJ monthly target:", json.dumps(cj, indent=2))
